9e0e6f54afb96e0948e026b321ca1e05d0eebd07
NRMilkTea <38323386+NRMilkTea@users.noreply.github.com>
2018-06-01 08:49:21 +0800

    Create getMax.s

---
+++ Reverse/getMax.s
@@ -0,0 +1,67 @@
+	.file	"getMax.c"
+	.section	.rodata
+.LC0:
+	.string	"Max value is : %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$100, -12(%rbp)
+	movl	$200, -8(%rbp)
+	movl	-8(%rbp), %edx
+	movl	-12(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	max
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.globl	max
+	.type	max, @function
+max:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	-20(%rbp), %eax
+	cmpl	-24(%rbp), %eax
+	jle	.L4
+	movl	-20(%rbp), %eax
+	movl	%eax, -4(%rbp)
+	jmp	.L5
+.L4:
+	movl	-24(%rbp), %eax
+	movl	%eax, -4(%rbp)
+.L5:
+	movl	-4(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	max, .-max
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.9) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
